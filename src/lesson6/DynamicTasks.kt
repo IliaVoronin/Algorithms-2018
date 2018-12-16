0929b4990c1bed7183ffb649a224d7017793cf83
Ilia Voronin <iliavoronin9898@gmail.com>
2018-12-16 18:27:52 +0300

    Update DynamicTasks.kt
    task 1 and 2

--- src/lesson6/DynamicTasks.kt
+++ src/lesson6/DynamicTasks.kt
@@ -12,9 +12,35 @@ package lesson6
  * (но по-прежнему должны быть расположены в исходной строке в том же порядке).
  * Если общей подпоследовательности нет, вернуть пустую строку.
  * При сравнении подстрок, регистр символов *имеет* значение.
+ * РЕСУРСОЕМКОСТЬ - o(N * M)
+ * ТРУДОЕМКОСТЬ - o(N * M)
+ * где N - длина первой строки, M - длина второй строки
  */
 fun longestCommonSubSequence(first: String, second: String): String {
-    TODO()
+    var output = ""
+    val max = Array(first.length + 1) { IntArray(second.length + 1) }
+    for (i in 1 until first.length + 1) {
+        for (j in 1 until second.length + 1) {
+            if (first[i - 1] == second[j - 1]) {
+                max[i][j] = 1 + max[i - 1][j - 1]
+            } else
+                max[i][j] = Math.max(max[i - 1][j], max[i][j - 1])
+        }
+    }
+    var i = first.length
+    var j = second.length
+    while (i > 0 && j > 0) {
+        when {
+            first[i - 1] == second[j - 1] -> {
+                output = first[i - 1] + output
+                i--
+                j--
+            }
+            max[i][j] == max[i - 1][j] -> i--
+            else -> j--
+        }
+    }
+    return output
 }
 
 /**
@@ -28,9 +54,38 @@ fun longestCommonSubSequence(first: String, second: String): String {
  * Если самых длинных возрастающих подпоследовательностей несколько (как в примере),
  * то вернуть ту, в которой числа расположены раньше (приоритет имеют первые числа).
  * В примере ответами являются 2, 8, 9, 12 или 2, 5, 9, 12 -- выбираем первую из них.
+ * РЕСУРСОЕМКОСТЬ - o(n)
+ * ТРУДОЕМКОСТЬ - o(n^2)
+ * ВСПОМОГАТЕЛЬНЫЙ МАТЕРИАЛ - http://e-maxx.ru/algo/longest_increasing_subseq_log
  */
 fun longestIncreasingSubSequence(list: List<Int>): List<Int> {
-    TODO()
+    val output = ArrayList<Int>()
+    if (list.isEmpty()) return output
+    val p = IntArray(list.size)
+    val d = IntArray(list.size)
+    for (i in 0 until list.size) {
+        d[i] = 1
+        p[i] = -1
+        for (j in 0 until i) {
+            if (list[j] < list[i] && d[j] + 1 > d[i]) {
+                d[i] = d[j] + 1
+                p[i] = j
+            }
+        }
+    }
+    var length = 0
+    var position = 0
+    for (i in d.indices) {
+        if (d[i] > length) {
+            position = i
+            length = d[i]
+        }
+    }
+    while (position != -1) {
+        output.add(0, list[position])
+        position = p[position]
+    }
+    return output
 }
 
 /**
